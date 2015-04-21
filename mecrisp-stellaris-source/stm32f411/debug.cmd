bcb0b91c2e271939b4070f5dbe881c7d92de62e3
Jean Jonethal <jjonethal@gmail.com>
2015-04-22 00:03:47 +0200

    adjust openocd path

--- mecrisp-stellaris-source/stm32f411/debug.cmd
+++ mecrisp-stellaris-source/stm32f411/debug.cmd
@@ -1,3 +1,5 @@
 :: E:\stm\openocd\openocd-0.9.0-dev-150204220259\openocd-0.9.0-dev-150204220259\scripts\
-start E:\stm\openocd\openocd-0.9.0-dev-150204220259\openocd-0.9.0-dev-150204220259\bin-x64\openocd-x64-0.9.0-dev-150204220259.exe -f E:\stm\openocd\openocd-0.9.0-dev-150204220259\openocd-0.9.0-dev-150204220259\scripts\board\st_nucleo_f4.cfg
+set PATH=C:\app\gcc\4.9_2014q4\bin;%PATH%
+start C:\app\openocd\bin-x64\openocd-x64-0.9.0-dev-150204220259.exe -f C:\app\openocd\scripts\board\st_nucleo_f4.cfg
+arm-none-eabi-gdb mecrisp-stellaris-stm32f411.elf -x .gdbinit
 pause
