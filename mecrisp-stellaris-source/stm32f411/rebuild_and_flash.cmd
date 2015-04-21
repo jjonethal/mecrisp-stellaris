57464643746be352f44e0b11d4d5bfd7b4d38466
Jean Jonethal <jean.jonethal@gigaset.com>
2015-04-21 13:40:45 +0200

    rename build-flash script, adapt to my desk

--- mecrisp-stellaris-source/stm32f411/rebuild_and_flash.cmd
+++ mecrisp-stellaris-source/stm32f411/rebuild_and_flash.cmd
@@ -1,5 +1,6 @@
 :: set PATH=C:\MinGW\bin;C:\MinGW\msys\1.0\bin;%PATH%
-set PATH=C:\gccarm\4.7_2014q2\bin;C:\MinGW\bin;C:\MinGW\msys\1.0\bin;%PATH%
+set GCC_DIR=E:\gcc\4.6_2012q4\bin
+set PATH=%GCC_DIR%;C:\MinGW\bin;C:\MinGW\msys\1.0\bin;%PATH%
 SET STLINK="C:\Program Files (x86)\STMicroelectronics\STM32 ST-LINK Utility\ST-LINK Utility\ST-LINK_CLI.exe"
 
 mingw32-make.exe clean all
