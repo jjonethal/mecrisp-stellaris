45403289dc413089da37b9bc3777ac6dc1d4f59b
Jean Jonethal <jean.jonethal@gigaset.com>
2015-04-21 17:48:24 +0200

    add debugger

---
+++ mecrisp-stellaris-source/stm32f411/debug.cmd
@@ -0,0 +1,3 @@
+:: E:\stm\openocd\openocd-0.9.0-dev-150204220259\openocd-0.9.0-dev-150204220259\scripts\
+start E:\stm\openocd\openocd-0.9.0-dev-150204220259\openocd-0.9.0-dev-150204220259\bin-x64\openocd-x64-0.9.0-dev-150204220259.exe -f E:\stm\openocd\openocd-0.9.0-dev-150204220259\openocd-0.9.0-dev-150204220259\scripts\board\st_nucleo_f4.cfg
+pause
