d7ef923953050afd650b3a22db20fb5aadf03bcc
Jean Jonethal <jjonethal@yahoo.de>
2015-04-22 21:26:41 +0200

    unify gnu arm path

--- mecrisp-stellaris-source/stm32f411/debug.cmd
+++ mecrisp-stellaris-source/stm32f411/debug.cmd
@@ -7,6 +7,11 @@ if "%COMPUTERNAME%" == "BCHF6KOC" (
 	set GNU_ARM_BIN=C:\app\gcc\4.9_2014q4\bin
 )
 
+if "%COMPUTERNAME%" == "GJ-PC" (
+	set OPEN_OCD_PATH=C:\app\openocd
+	set GNU_ARM_BIN=C:\gccarm\4.7_2014q2\bin
+)
+
 set OPEN_OCD_EXE=openocd-x64-0.9.0-dev-150204220259.exe
 set OPEN_OCD_CMD=%OPEN_OCD_PATH%\bin-x64\%OPEN_OCD_EXE%
 
