d29068a30a755ce4dfa8b76b58adaeb058e6a5ea
Jean Jonethal <jjonethal@yahoo.de>
2015-04-22 21:27:09 +0200

    unify variables

--- mecrisp-stellaris-source/stm32f411/rebuild_and_flash.cmd
+++ mecrisp-stellaris-source/stm32f411/rebuild_and_flash.cmd
@@ -1,12 +1,17 @@
 SET STLINK="C:\Program Files (x86)\STMicroelectronics\STM32 ST-LINK Utility\ST-LINK Utility\ST-LINK_CLI.exe"
 
 if "%COMPUTERNAME%" == "BCHF6KOC" (
-	set GCC_BIN=E:\gcc\4.6_2012q4\bin
+	set GNU_ARM_BIN=E:\gcc\4.6_2012q4\bin
 ) else (
-	set GCC_BIN=C:\app\gcc\4.9_2014q4\bin
+	set GNU_ARM_BIN=C:\app\gcc\4.9_2014q4\bin
 )
 
-set PATH=%GCC_BIN%;C:\MinGW\bin;C:\MinGW\msys\1.0\bin;%PATH%
+if "%COMPUTERNAME%" == "GJ-PC" (
+	set GNU_ARM_BIN=C:\gccarm\4.7_2014q2\bin
+)
+
+
+set PATH=%GNU_ARM_BIN%;C:\MinGW\bin;C:\MinGW\msys\1.0\bin;%PATH%
 
 mingw32-make.exe clean all
 
