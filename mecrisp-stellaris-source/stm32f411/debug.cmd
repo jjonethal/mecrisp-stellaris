fe9a04f762ba74b577ba856de039b24be1642f7a
Jean Jonethal <jean.jonethal@gigaset.com>
2015-04-22 08:48:28 +0200

    avoid to many changes

--- mecrisp-stellaris-source/stm32f411/debug.cmd
+++ mecrisp-stellaris-source/stm32f411/debug.cmd
@@ -1,5 +1,17 @@
-:: E:\stm\openocd\openocd-0.9.0-dev-150204220259\openocd-0.9.0-dev-150204220259\scripts\
-set PATH=C:\app\gcc\4.9_2014q4\bin;%PATH%
-start C:\app\openocd\bin-x64\openocd-x64-0.9.0-dev-150204220259.exe -f C:\app\openocd\scripts\board\st_nucleo_f4.cfg
+
+if "%COMPUTERNAME%" == "BCHF6KOC" (
+	set OPEN_OCD_PATH=E:\stm\openocd\openocd-0.9.0-dev-150204220259\openocd-0.9.0-dev-150204220259
+	set GNU_ARM_BIN=E:\gcc\4.6_2012q4\bin
+) else (
+	set OPEN_OCD_PATH=C:\app\openocd
+	set GNU_ARM_BIN=C:\app\gcc\4.9_2014q4\bin
+)
+
+set OPEN_OCD_EXE=openocd-x64-0.9.0-dev-150204220259.exe
+set OPEN_OCD_CMD=%OPEN_OCD_PATH%\bin-x64\%OPEN_OCD_EXE%
+
+
+set PATH=%GNU_ARM_BIN%;%PATH%
+start %OPEN_OCD_CMD% -f %OPEN_OCD_PATH%\scripts\board\st_nucleo_f4.cfg
 arm-none-eabi-gdb mecrisp-stellaris-stm32f411.elf -x .gdbinit
 pause
--- mecrisp-stellaris-source/stm32f411/rebuild_and_flash.cmd
+++ mecrisp-stellaris-source/stm32f411/rebuild_and_flash.cmd
@@ -1,7 +1,12 @@
-:: set PATH=C:\MinGW\bin;C:\MinGW\msys\1.0\bin;%PATH%
-set GCC_DIR=C:\app\gcc\4.9_2014q4\bin
-set PATH=%GCC_DIR%;C:\MinGW\bin;C:\MinGW\msys\1.0\bin;%PATH%
 SET STLINK="C:\Program Files (x86)\STMicroelectronics\STM32 ST-LINK Utility\ST-LINK Utility\ST-LINK_CLI.exe"
+
+if "%COMPUTERNAME%" == "BCHF6KOC" (
+	set GCC_BIN=E:\gcc\4.6_2012q4\bin
+) else (
+	set GCC_BIN=C:\app\gcc\4.9_2014q4\bin
+)
+
+set PATH=%GCC_BIN%;C:\MinGW\bin;C:\MinGW\msys\1.0\bin;%PATH%
 
 mingw32-make.exe clean all
 
