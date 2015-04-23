f188985beccdca3364c6fb4afc7b45a1027090bf
Jean Jonethal <jjonethal@yahoo.de>
2015-04-23 22:30:48 +0200

    automatic file download

--- stm32f411/teraterm.cmd
+++ stm32f411/teraterm.cmd
@@ -1,11 +1,25 @@
 :: tt.cmd
-for %%I in (adc.txt) do set F=%%~fI
-echo CRReceive=CR+LF > macro.ttl
+:: select comport
+set COMPORT=16
+
+:: for %%I in (adc.txt) do set F=%%~fI
+if not "%1" == "" set F=%~f1
+echo.   > macro.ttl
 :: echo callmenu 50310 >> macro.ttl
+echo sendln 'reset' >> macro.ttl
 echo sendfile '%F%' 0 >> macro.ttl
+:: echo setsync 1 >> macro.ttl
+echo timeout=2 >> macro.ttl
+echo do >> macro.ttl
+echo   result=0 >> macro.ttl
+echo   wait #10 #13 >> macro.ttl
+echo loop while result ^> 0 >> macro.ttl
+echo dispstr "finished" >> macro.ttl
+echo closett >> macro.ttl
+echo. >> macro.ttl
 
 for %%I in (macro.ttl) do set M=%%~fI
+for %%I in (tt_txcrlf.ini) do set INI=%%~fI
 
-pause
-
-start C:\app\teraterm\ttermpro.exe /BAUD=115200 /C=16 /FD="%CD%" /M="%M%"
+C:\app\teraterm\ttermpro.exe /BAUD=115200 /C=%COMPORT% /FD="%CD%" /M="%M%" /F="%INI%"
+start C:\app\teraterm\ttermpro.exe /BAUD=115200 /C=%COMPORT%
