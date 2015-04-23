2aed3a80b78a0ecf6073c36aa6a76d19ce4bf3ad
Jean Jonethal <jjonethal@yahoo.de>
2015-04-23 07:19:33 +0200

    no need for computernames

--- mecrisp-stellaris-source/stm32f411/rebuild_and_flash.cmd
+++ mecrisp-stellaris-source/stm32f411/rebuild_and_flash.cmd
@@ -1,14 +1,11 @@
 SET STLINK="C:\Program Files (x86)\STMicroelectronics\STM32 ST-LINK Utility\ST-LINK Utility\ST-LINK_CLI.exe"
 
-if "%COMPUTERNAME%" == "BCHF6KOC" (
-	set GNU_ARM_BIN=E:\gcc\4.6_2012q4\bin
-) else (
-	set GNU_ARM_BIN=C:\app\gcc\4.9_2014q4\bin
-)
-
-if "%COMPUTERNAME%" == "GJ-PC" (
-	set GNU_ARM_BIN=C:\gccarm\4.7_2014q2\bin
-)
+SET P=E:\gcc\4.6_2012q4\bin
+if exist %P%\nul set GNU_ARM_BIN=%P%
+SET P=C:\gccarm\4.7_2014q2\bin
+if exist %P%\nul set GNU_ARM_BIN=%P%
+SET P=C:\app\gcc\4.9_2014q4\bin
+if exist %P%\nul set GNU_ARM_BIN=%P%
 
 
 set PATH=%GNU_ARM_BIN%;C:\MinGW\bin;C:\MinGW\msys\1.0\bin;%PATH%
