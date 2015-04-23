8f16b2870aaf3769c30bef2c5fed47760843e988
Jean Jonethal <jjonethal@yahoo.de>
2015-04-23 23:07:09 +0200

    rename teratem to sendfile

--- stm32f411/sendfile.cmd
+++ stm32f411/sendfile.cmd
@@ -5,8 +5,15 @@ set COMPORT=16
 :: for %%I in (adc.txt) do set F=%%~fI
 if not "%1" == "" set F=%~f1
 echo.   > macro.ttl
+echo testlink >> macro.ttl
+echo if result ^> 0 closett >> macro.ttl
+for %%I in (macro.ttl) do set M=%%~fI
+for %%I in (tt_txcrlf.ini) do set INI=%%~fI
+C:\app\teraterm\ttpmacro.exe "%M%"
+
+
 :: echo callmenu 50310 >> macro.ttl
-echo sendln 'reset' >> macro.ttl
+echo sendln 'reset' > macro.ttl
 echo sendfile '%F%' 0 >> macro.ttl
 :: echo setsync 1 >> macro.ttl
 echo timeout=2 >> macro.ttl
@@ -22,4 +29,6 @@ for %%I in (macro.ttl) do set M=%%~fI
 for %%I in (tt_txcrlf.ini) do set INI=%%~fI
 
 C:\app\teraterm\ttermpro.exe /BAUD=115200 /C=%COMPORT% /FD="%CD%" /M="%M%" /F="%INI%"
-start C:\app\teraterm\ttermpro.exe /BAUD=115200 /C=%COMPORT%
+:: C:\app\teraterm\ttpmacro.exe "%M%"
+
+:: start C:\app\teraterm\ttermpro.exe /BAUD=115200 /C=%COMPORT%
