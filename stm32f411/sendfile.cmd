7bb44f7c013767bd5f4c8a3ff82b88d68f77ab08
Jean Jonethal <jjonethal@yahoo.de>
2015-04-28 20:33:25 +0200

    introduce variables for settings add comments

--- stm32f411/sendfile.cmd
+++ stm32f411/sendfile.cmd
@@ -1,15 +1,22 @@
-:: tt.cmd
+:: sendfile.cmd
+:: send a forth source file using teraterm
+:: sendfile file1.txt file2.txt file3.txt 
+:: without parameter start teraterm on COMPORT
 :: select comport
 set COMPORT=16
+set TERA_TERM_DIR=C:\app\teraterm
 
+:: loop over file list ignore non-txt files
 :scanfiles
 if "%1" == "" goto ttstart
 if "%~sx1" == ".txt" call :send %1
+if "%~sx1" == ".TXT" call :send %1
 shift
 goto scanfiles
 
+:: no (more) parameters start teraterm
 :ttstart
-start C:\app\teraterm\ttermpro.exe /BAUD=115200 /C=%COMPORT%
+start %TERA_TERM_DIR%\ttermpro.exe /BAUD=115200 /C=%COMPORT%
 goto :EOF
 
 :send
@@ -24,11 +31,12 @@ echo wait #10 #13 >> macro.ttl
 echo closett >> macro.ttl
 for %%I in (macro.ttl) do set M=%%~fI
 for %%I in (tt_txcrlf.ini) do set INI=%%~fI
-C:\app\teraterm\ttpmacro.exe "%M%"
+%TERA_TERM_DIR%\ttpmacro.exe "%M%"
 
 
 :: echo callmenu 50310 >> macro.ttl
-echo sendln 'reset' > macro.ttl
+echo sendln ' ' > macro.ttl
+:: echo sendln 'reset' > macro.ttl
 echo sendfile '%F%' 0 >> macro.ttl
 :: echo setsync 1 >> macro.ttl
 echo timeout=2 >> macro.ttl
@@ -43,5 +51,5 @@ echo. >> macro.ttl
 for %%I in (macro.ttl) do set M=%%~fI
 for %%I in (tt_txcrlf.ini) do set INI=%%~fI
 
-C:\app\teraterm\ttermpro.exe /BAUD=115200 /C=%COMPORT% /FD="%CD%" /M="%M%" /F="%INI%"
+%TERA_TERM_DIR%\ttermpro.exe /BAUD=115200 /C=%COMPORT% /FD="%CD%" /M="%M%" /F="%INI%"
 goto :EOF
