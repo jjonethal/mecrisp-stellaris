847e20b800e40680acc3e6563b649c9437384b66
Jean Jonethal <jjonethal@yahoo.de>
2015-05-13 06:37:09 +0200

    use variable for macro.ttl, store macro in same folder as sendfile.cmd

--- stm32f411/sendfile.cmd
+++ stm32f411/sendfile.cmd
@@ -3,8 +3,9 @@
 :: sendfile file1.txt file2.txt file3.txt 
 :: without parameter start teraterm on COMPORT
 :: select comport
-set COMPORT=15
+set COMPORT=16
 set TERA_TERM_DIR=C:\app\teraterm
+set MACRO=%~dp0macro.ttl
 
 :: loop over file list ignore non-txt files
 :scanfiles
@@ -23,32 +24,32 @@ goto :EOF
 echo sending %1
 :: for %%I in (adc.txt) do set F=%%~fI
 if not "%1" == "" set F=%~f1
-echo.   > macro.ttl
-echo connect '/C=%COMPORT%' >> macro.ttl
-echo timeout=10 >> macro.ttl
-echo callmenu 50200 >> macro.ttl
-echo wait #10 #13 >> macro.ttl
-echo closett >> macro.ttl
-for %%I in (macro.ttl) do set M=%%~fI
-for %%I in (tt_txcrlf.ini) do set INI=%%~fI
+echo.   > %MACRO%
+echo connect '/C=%COMPORT%' >> %MACRO%
+echo timeout=10 >> %MACRO%
+echo callmenu 50200 >> %MACRO%
+echo wait #10 #13 >> %MACRO%
+echo closett >> %MACRO%
+for %%I in (%MACRO%) do set M=%%~fI
+for %%I in (%~dp0tt_txcrlf.ini) do set INI=%%~fI
 %TERA_TERM_DIR%\ttpmacro.exe "%M%"
 
 :: echo callmenu 50310 >> macro.ttl
-echo. > macro.ttl
-echo sendln 'reset' >> macro.ttl
-echo sendfile '%F%' 0 >> macro.ttl
-:: echo setsync 1 >> macro.ttl
-echo timeout=2 >> macro.ttl
-echo do >> macro.ttl
-echo   result=0 >> macro.ttl
-echo   wait #10 #13 >> macro.ttl
-echo loop while result ^> 0 >> macro.ttl
-echo dispstr "finished" >> macro.ttl
-echo closett >> macro.ttl
-echo. >> macro.ttl
+echo. > %MACRO%
+:: echo sendln 'reset' >> %MACRO%
+echo sendfile '%F%' 0 >> %MACRO%
+:: echo setsync 1 >> %MACRO%
+echo timeout=2 >> %MACRO%
+echo do >> %MACRO%
+echo   result=0 >> %MACRO%
+echo   wait #10 #13 >> %MACRO%
+echo loop while result ^> 0 >> %MACRO%
+echo dispstr "finished" >> %MACRO%
+echo closett >> %MACRO%
+echo. >> %MACRO%
 
-for %%I in (macro.ttl) do set M=%%~fI
-for %%I in (tt_txcrlf.ini) do set INI=%%~fI
+for %%I in (%MACRO%) do set M=%%~fI
+for %%I in (%~dp0tt_txcrlf.ini) do set INI=%%~fI
 
 %TERA_TERM_DIR%\ttermpro.exe /BAUD=115200 /C=%COMPORT% /FD="%CD%" /M="%M%" /F="%INI%"
 goto :EOF
