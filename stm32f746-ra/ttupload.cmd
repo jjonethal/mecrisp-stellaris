14fd2c73a82ad0465ad49c4cc045bbd93e4ca1f1
Jean Jonethal <jjonethal@yahoo.de>
2021-12-27 21:13:09 +0100

    change commport

--- stm32f746-ra/ttupload.cmd
+++ stm32f746-ra/ttupload.cmd
@@ -12,7 +12,7 @@
 
 
 :: comport number for connection
-set COMPORT=4
+set COMPORT=3
 :: tempoary macrco for closing down existing connection
 set MACRO=%~dp0tempmac.ttl
 :: Teraterm installation directory
