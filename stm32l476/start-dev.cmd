385a70b4abade96d340a86d1c43d684444131972
Jean Jonethal <jjonethal@yahoo.de>
2021-12-29 14:19:54 +0100

    update documentation launcher

--- stm32l476/start-dev.cmd
+++ stm32l476/start-dev.cmd
@@ -1,6 +1,7 @@
-start "C:\Users\jeanjo\Downloads\stm\stm32l476 discovery\n25q_128mb_3v_65nm.pdf"
-start "C:\Users\jeanjo\Downloads\stm\stm32l476 discovery\en.DM00036465.pdf"
-start "C:\Users\jeanjo\Downloads\stm\stm32l476 discovery\en.DM00089896.pdf"
-start "C:\Users\jeanjo\Downloads\stm\DM00172179 UM1879 User manual Discovery kit with STM32L476VG MCU.pdf"
-start "C:\Users\jeanjo\Downloads\stm\en.DM00083560 RM0351 STM32L4x6 advanced ARM®-based 32-bit MCUs .pdf"
-start "C:\Users\jeanjo\Downloads\stm\en.DM00108832 STM32L476xx datasheet .pdf"
+start "" "C:\Users\jeanjo\Downloads\stm\stm32l476 discovery\n25q_128mb_3v_65nm.pdf"
+start "" "C:\Users\jeanjo\Downloads\stm\l3gd20\l3gd20.pdf"
+start "" "C:\Users\jeanjo\Downloads\stm\lsm303c\lsm303c.pdf"
+start "" "C:\Users\jeanjo\Downloads\stm\stm32l476\um1879-discovery-kit-with-stm32l476vg-mcu-stmicroelectronics.pdf"
+start "" "C:\Users\jeanjo\Downloads\stm\stm32l476\rm0351-stm32l47xxx-stm32l48xxx-stm32l49xxx-and-stm32l4axxx-advanced-armbased-32bit-mcus-stmicroelectronics.pdf"
+start "" "C:\Users\jeanjo\Downloads\stm\stm32l476\stm32l476vg.pdf"
+start "" https://www.st.com/en/microcontrollers-microprocessors/stm32l476vg.html#documentation
