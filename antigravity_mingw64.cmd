40cdd39b2411c0d73ff4ab4fbe6c7f599ee6f353
Jean Jonethal <jjonethal@gmail.com>
2026-02-16 20:12:38 +0100

    update build environment

--- antigravity_mingw64.cmd
+++ antigravity_mingw64.cmd
@@ -2,6 +2,6 @@ setlocal
 
 cd /D %~dp0
 set MSYSTEM=MINGW64
-set "PATH=%PATH%;C:\msys64\mingw64\bin;C:\msys64\usr\local\bin"
+set "PATH=C:\msys64\mingw64\bin;C:\msys64\mingw64\local\bin;C:\msys64\usr\local\bin;C:\msys64\usr\bin;%PATH%"
 start "" "C:\Users\jjone\AppData\Local\Programs\Antigravity\Antigravity.exe" .
 call "C:\msys64\msys2_shell.cmd" -here
